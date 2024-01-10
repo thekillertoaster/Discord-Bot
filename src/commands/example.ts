a15d28f508b9313bf3421d7ca876a029906d4cb0
Aaron Hands <thekillertoaster@gmail.com>
2024-01-10 22:09:15 +0000

    removed tsignore

--- src/commands/example.ts
+++ src/commands/example.ts
@@ -6,7 +6,7 @@ const command: Command = {
         name: 'example',
         description: 'This is an example command!'
     },
-    // @ts-ignore
+
     async execute(interaction) {
         await interaction.reply('Hello from the example command!');
     },
